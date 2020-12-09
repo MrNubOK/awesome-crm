67e3d5e55a8c48f4a5f556b3bdab5f8e24861583
MrNubOk <tibets1995@mail.ru>
2020-12-09 20:21:04 +0300

    lesson 2

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,86 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/Login'),
+        meta: {
+            layout: 'empty'
+        }
+    },
+    {
+        path: '/registration',
+        name: 'registration',
+        component: () => import('../views/Registration'),
+        meta: {
+            layout: 'empty'
+        }
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: () => import('../views/Home'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/categories',
+        name: 'categories',
+        component: () => import('../views/Categories'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/history',
+        name: 'history',
+        component: () => import('../views/History'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/planning',
+        name: 'planning',
+        component: () => import('../views/Planning'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('../views/Profile'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('../views/Profile'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/record',
+        name: 'record',
+        component: () => import('../views/Record'),
+        meta: {
+            layout: 'main'
+        }
+    },
+    {
+        path: '/detail-record',
+        name: 'detail-record',
+        component: () => import('../views/DetailRecord'),
+        meta: {
+            layout: 'main'
+        }
+    },
 ]
 
 const router = new VueRouter({
